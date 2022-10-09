2ffdf17729bcdebd6247e406c5543fedeb1f6045
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-10-09 10:38:18 +0200

    new ddl and dump file

--- scripts/entrypoints/surface.sh
+++ scripts/entrypoints/surface.sh
@@ -1,1 +1,4 @@
-PGPASSWORD=password psql -U postgres postgres -f /schemas/surface_dump.sql
+PGPASSWORD=password psql -h 127.0.0.1 -p 5432 -U postgres postgres -f /schemas/1.sql
+PGPASSWORD=password psql -h 127.0.0.1 -p 5432 -U postgres postgres -f /schemas/2.sql
+PGPASSWORD=password psql -h 127.0.0.1 -p 5432 -U postgres postgres -f /schemas/3.sql
+PGPASSWORD=password psql -h 127.0.0.1 -p 5432 -U postgres postgres -f /schemas/4.sql
