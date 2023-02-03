fa184c7f3ef20ad734f2f1227b8724d45bb83265
Chinedu <chinedudimonyeka@gmail.com>
2023-02-03 18:32:56 +0100

    Edits setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.readlines()
 
 setup(
     name='opencdms_test_data',
     version='0.1.0',
     description='OpenCDMS test data as package.',
+    entry_points={
+        "console_scripts": [
+            "opencdms-test-data=cli:main",
+        ],
+    },
+    install_requires=requirements,
     author='OpenCDMS',
     author_email='info@opencdms.org',
     url='https://github.com/opencdms/opencdms-test-data',
